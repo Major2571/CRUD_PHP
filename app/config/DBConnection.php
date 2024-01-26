0003e44ecda856bc931b983959d5330127e83a97
Major2571 <caroline.tacats62@gmail.com>
2024-01-26 17:02:02 -0300

    Edit DB name and add charset utf8

--- app/config/DBConnection.php
+++ app/config/DBConnection.php
@@ -1,17 +1,17 @@
 <?php
-
-
-class Connection {
+class Connection
+{
     protected $connection;
     private $servername = "localhost";
-    private $database = "dbCRUD-PHP";
+    private $database = "dbCRUD_PHP";
     private $username = "root";
     private $password = "";
 
     function connectDataBase()
     {
         try {
-            $this->connection = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+            $pdo = $this->connection = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+            $pdo->exec('SET CHARACTER SET utf8');
         } catch (PDOException $e) {
             throw new Exception("Erro na conexão com o banco de dados: " . $e->getMessage());
         }
