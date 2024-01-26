17a1a6fd72c00eaf88ae81afbadd19fcf8df23d0
Major2571 <caroline.tacats62@gmail.com>
2024-01-26 17:04:14 -0300

    Refactor controller action handling for better security and readability

--- app/routes/Routes.php
+++ app/routes/Routes.php
@@ -1,23 +1,44 @@
 <?php
+
 require_once('./app/controllers/UserController.php');
 
 $controller = new UserController();
 
-$action = (isset($_GET['a']) || isset($_POST['a'])) ? (isset($_POST['a']) ? $_POST['a'] : $_GET['a']) : 'getAll';
+$action = $_GET['a'] ?? $_POST['a'] ?? 'getAll';
 
+try {
+    switch ($action) {
 
-if ($action == "deleteUser") {
-    $id = $_GET['id'];
-    $controller->deleteUser($id);
-} else if ($action == "insertUser") {
-    $controller->insertUser($_POST);
-} else if ($action == "updateUser") {
-    $controller->updateUser($_POST);
-} else if ($action == "editUser") {
-    $userId = isset($_GET['id']) ? $_GET['id'] : null;
-        if ($userId) {
-            $controller->editUser($userId);
-        }
-} else {
-    $controller->{$action}();
+        case 'insertUser':
+            $controller->insertUser($_POST);
+            break;
+
+        case 'updateUser':
+            $controller->updateUser($_POST);
+            break;
+
+        case 'editUser':
+            $userId = $_GET['id'] ?? null;
+            if ($userId !== null) {
+                $controller->editUser($userId);
+            } else {
+                throw new Exception('O usuário não existe!');
+            }
+            break;
+
+        case 'deleteUser':
+            $id = $_GET['id'] ?? null;
+            if ($id !== null) {
+                $controller->deleteUser($id);
+            } else {
+                throw new Exception('O usuário não existe!');
+            }
+            break;
+
+        default:
+            $controller->{$action}();
+            break;
+    }
+} catch (Exception $e) {
+    echo 'Erro: ' . $e->getMessage();
 }
